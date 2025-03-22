ca9366d630cf0bb0525fdec37118725701228531
mitulsavani100 <mitulsavani100@gmail.com>
2025-03-22 12:19:02 +0530

    set header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import React from "react";
+import Background from "/main-background.png"
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Route, Routes } from "react-router";
 import Home from "./pages/Home";
 import About from "./pages/about";
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import Cursor from "./components/cursor";
 
 function App() {
+  useGSAP(() => {
+    // gsap code here...
+    gsap.from('.main-background', { 
+      opacity: 0,
+      duration: 1,
+    }); // <-- automatically reverted
+  },); // <-- scope is for selector text (optional)
   return (
     <>
       <Router>
+        <Cursor/>
+        <img src={Background} alt="Background" className="absolute top-0 inset-x-0 -z-10 main-background" />
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,45 @@
 import React from "react";
 import Logo from "/logo.svg";
 import { Link, NavLink } from "react-router-dom";
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
 
 const Navbar = () => {
+  useGSAP(() => {
+    // gsap code here...
+    gsap.from('header *', { 
+      y: 50,
+      opacity: 0,
+      duration: 0.6,
+      stagger:0.2, 
+      delay: 0.2
+    }); // <-- automatically reverted
+  },); // <-- scope is for selector text (optional)
   return (
     <>
-      <header>
-        <div className="container">
-          <div className="logo">
+      <div className="container py-5">
+        <header className="bg-white rounded-full p-2.5 flex items-center justify-between overflow-hidden">
+          <div className="logo pl-5">
             <NavLink to="/">
-              <img src={Logo} alt="logo" />
+              <img src={Logo} alt="logo" className="w-[160px]" />
             </NavLink>
           </div>
-          <div className="flex">
+          <nav className="flex uppercase font-medium text-base space-x-11">
             <NavLink activeclassname="active" to="/" className="">
               Home
             </NavLink>
-          </div>
-        </div>
-      </header>
+            <NavLink activeclassname="active" to="/about" className="">
+              About
+            </NavLink>
+            <NavLink activeclassname="active" to="/services" className="">
+              Services
+            </NavLink>
+          </nav>
+          <NavLink activeclassname="active" to="/contact-us" className="bg-primary text-white font-bold text-lg py-5 px-9 rounded-full">
+            Get into touch
+          </NavLink>
+        </header>
+      </div>
     </>
   );
 };
