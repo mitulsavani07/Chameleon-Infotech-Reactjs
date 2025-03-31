2c1ffa7ee6beb8603bff16723099e7c46962f6e9
mitulsavani100 <mitulsavani100@gmail.com>
2025-03-31 17:22:06 +0530

    scrolling card

--- src/sections/ScrollingCard.jsx
+++ src/sections/ScrollingCard.jsx
@@ -49,53 +49,94 @@ const cardData = [
 
 function ScrollingCard() {
   const cardsRef = useRef(null);
+  let scrollTrigger; // Declare scrollTrigger outside of useEffect
 
   useEffect(() => {
     const races = cardsRef.current;
-    const card = document.querySelector('.card')
-    const bannerSection = document.querySelector('#bannerSection') 
+    const card = gsap.utils.toArray('.card');
+    const cardWrapper = gsap.utils.toArray('.cardWrapper');
+    const bannerSection = document.querySelector('.brands-imgs');
+    const mm = gsap.matchMedia();
+
     const getScrollAmount = () => {
       let racesWidth = races.scrollWidth;
       return -(racesWidth - window.innerWidth);
     };
 
-    gsap.set(card, {
-      trigger: bannerSection,
-      xPercent: 100,
-      x: (i) => -i * 350,
-      rotate: 10,
-      zIndex: (i) => card.length - i,
-      transformOrigin: "bottom center",
-      force3D: true,
-  });
+    mm.add("(min-width:1024px)", () => {
+      gsap.set(card, {
+        trigger: bannerSection,
+        xPercent: 0,
+        x: (i) => -i * 350,
+        rotate: 10,
+        zIndex: (i) => card.length - i,
+        transformOrigin: "bottom bottom",
+        force3D: true,
+      });
 
-    const tween = gsap.to(races, {
-      transform: "translateX(-100%)",
-      duration: 3,
-      ease: "none",
-    });
+      gsap.to(card, {
+        x: (i) => i * 0,
+        rotate: 0,
+        ease: "power1.out",
+        scrollTrigger: {
+          trigger: bannerSection,
+          pin: true,
+          scrub: 5,
+          start: "bottom top",
+          end: "+=" + (cardWrapper.offsetWidth * 0.1),
+          onUpdate: (self) => {
+            if (self.progress < 0.1) {
+              gsap.set(card, {
+                rotate: 10,
+              });
+            } else if (self.progress >= 0.1 && self.progress < 1) {
+              gsap.set(card, {
+                rotate: 0,
+              });
+            }
+          }
+        }
+      });
 
-    const scrollTrigger = ScrollTrigger.create({
-      trigger: ".cardWrapper",
-      start: "top top",
-      end: "top -100%",
-      // end: () => `+=${getScrollAmount() * -1}`,
-      pin: true,
-      animation: tween,
-      scrub: 5,
-      invalidateOnRefresh: true,
-      markers: true,
+      const tween = gsap.to(races, {
+        transform: "translateX(-50%)",
+        duration: 3,
+        ease: "none",
+      });
+
+      scrollTrigger = ScrollTrigger.create({ // Assign to the outer variable
+        trigger: ".cardWrapper",
+        start: "top top",
+        end: "top -100%",
+        pin: true,
+        animation: tween,
+        scrub: 5,
+        invalidateOnRefresh: true,
+        markers: false,
+      });
+
+      const cardHoverAnimation = (item) => {
+        item.addEventListener('mouseenter', () => {
+          gsap.to(item, { y: -10, duration: 0.3, ease: 'power1.out' });
+        });
+        item.addEventListener('mouseleave', () => {
+          gsap.to(item, { y: 0, duration: 0.3, ease: 'power1.out' });
+        });
+      };
+
+      card.forEach(card => cardHoverAnimation(card));
     });
 
     return () => {
-      scrollTrigger.kill(); // Cleanup on unmount
-      tween.kill(); // Cleanup tween on unmount
+      if (scrollTrigger) {
+        scrollTrigger.kill(); // Cleanup on unmount
+      }
     };
   }, []);
 
   return (
-    <div className="overflow-hidden card-main h-dvh flex flex-col justify-center items-center cardWrapper">
-      <div className="container my-36">
+    <div className="overflow-hidden card-main flex flex-col justify-center items-center cardWrapper">
+      <div className="container my-10 md:my-36">
         <div className="max-w-[730px] w-full mx-auto text-center">
           <h2 className="text-2xl md:text-3xl lg:text-5xl font-bold">
             Empowering Businesses with Cutting-Edge IT Solutions
@@ -106,12 +147,12 @@ function ScrollingCard() {
         </div>
         <div
           ref={cardsRef}
-          className="flex mt-16 flex-nowrap space-x-9 snap-x snap-proximity cards"
+          className="flex mt-10 md:mt-16 flex-wrap lg:flex-nowrap cards md:-mx-3 lg:-mx-6"
         >
           {cardData.map((card) => (
             <div
               key={card.id}
-              className="py-11 px-7 rounded-3xl min-w-[320px] snap-center"
+              className="py-11 px-7 rounded-3xl my-3 md:m-3 lg:my-0 lg:mx-6 w-full md:w-[calc(50%-24px)] lg:w-auto lg:min-w-[320px] card"
               style={{ backgroundColor: card.bgColor, color: card.color }}
             >
               <span className="w-9 h-9 flex items-center justify-center bg-white rounded-full font-bold">
