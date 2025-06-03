6b6160a0c5a31b57ffe48b2f4bef0c71741f4b87
Mitul Savani <100030311+mitulsavani07@users.noreply.github.com>
2025-06-03 16:04:40 +0530

    change the image

--- src/data/successStories.js
+++ src/data/successStories.js
@@ -2,25 +2,25 @@ export const successStories = [
   {
     id: 1,
     title: "Everprint",
-    image: "/success-stories/everprint-1.png",
+    image: "/success-stories/everprint-1.jpg",
     features: ["React", "Node.js", "MongoDB", "AWS"],
   },
   {
     id: 2,
     title: "Everprint",
-    image: "/success-stories/everprint-2.png",
+    image: "/success-stories/everprint-2.jpg",
     features: ["Laravel", "MySQL", "Razorpay", "Bootstrap"],
   },
   {
     id: 3,
     title: "Everprint",
-    image: "/success-stories/everprint-3.png",
+    image: "/success-stories/everprint-3.jpg",
     features: ["Shopify Custom App", "Three.js", "Figma"],
   },
   {
     id: 4,
     title: "Everprint",
-    image: "/success-stories/everprint-4.png",
+    image: "/success-stories/everprint-4.jpg",
     features: ["Angular", "Firebase", "REST API", "Google Auth"],
   },
 ];
